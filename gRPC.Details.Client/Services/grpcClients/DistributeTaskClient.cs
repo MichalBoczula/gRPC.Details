0bedaabf57d42ef76e67bf895a275152b07c98b4
Michal Boczula <michal.boczula0101@gmail.com>
2022-12-25 14:58:18 +0100

    Added client

---
+++ gRPC.Details.Client/Services/grpcClients/DistributeTaskClient.cs
@@ -0,0 +1,30 @@
+﻿using gRPC.Details.Server.Protos.DistributeTask;
+using Grpc.Net.Client;
+
+namespace gRPC.Details.Client.Services.grpcClients
+{
+    public class DistributeTaskClient : BackgroundService
+    {
+        private DistributeTaskService.DistributeTaskServiceClient _client;
+
+
+        protected DistributeTaskService.DistributeTaskServiceClient Client
+        {
+            get
+            {
+                if (this._client == null)
+                {
+                    var channel = GrpcChannel.ForAddress("");
+                    this._client = new DistributeTaskService.DistributeTaskServiceClient(channel);
+                }
+                return this._client;
+            }
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            var task = new AddedTask();
+            var result = await Client.AddTaskAsync(task);
+        }
+    }
+}
